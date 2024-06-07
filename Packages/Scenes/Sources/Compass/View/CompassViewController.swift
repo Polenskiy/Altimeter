80e758265fbfdbd27c29aecc3062d72c7bf01fc6
Daniil Polenskii <polenskii13@mail.ru>
2024-06-07 18:10:18 +0300

    Added base compass view

--- Packages/Scenes/Sources/Compass/View/CompassViewController.swift
+++ Packages/Scenes/Sources/Compass/View/CompassViewController.swift
@@ -11,6 +11,7 @@ final class CompassViewController: UIViewController {
     
     var output: CompassViewControllerOutput?
     
+    let compassView: CompassView = CompassView()
     // MARK: - Functions
     
     override func viewDidLoad() {
@@ -23,6 +24,20 @@ final class CompassViewController: UIViewController {
 // MARK: - CompassViewControllerInput
 extension CompassViewController: CompassViewControllerInput {
     func setupInitialState() {
-        view.backgroundColor = .systemPink
+        view.backgroundColor = UIColor(named: "darkBlue")
+        configureCompassView()
+    }
+}
+
+private extension CompassViewController {
+    func configureCompassView() {
+        view.addSubview(compassView)
+              compassView.translatesAutoresizingMaskIntoConstraints = false
+              NSLayoutConstraint.activate([
+                  compassView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+                  compassView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+                  compassView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                  compassView.heightAnchor.constraint(equalToConstant: view.frame.width - 32)
+              ])
     }
 }
